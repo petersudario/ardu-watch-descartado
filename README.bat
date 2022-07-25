112b2192eedc6bbc1eeffc614c84ece1c5dc7543
Pedro Henrique Sudario da Silva <84980739+petersudario@users.noreply.github.com>
2022-07-25 11:52:24 -0300

    Leia Me
    Etapas para instalar o projeto

--- README.bat
+++ README.bat
@@ -17,32 +17,6 @@ Etapas para instalação da Node_modules:
 
 -- Parte React
 1. cd ardu-watch
-2. npm install -s
-3. npm i react 
-4. npm i reactjs
-5. npm i react-datepicker
-6. npm i react-dom
-7. npm i react-router-dom
-
--- Backend
-1. cd backend
-2. npm install -s
-3. npm i body-parser express cors mongoose dotenv
-4. Acesse o package.json da pasta backend
-
-5. Troque a parte scripts pelo seguinte: 
- "scripts": {
-    "serve": "node server.js"
-  },
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  Isto vai servir para rodar o servidor.
+2. npm install --save
+3. npm install axios react react-dom react-router-dom react-datepicker react-scripts
 #>
